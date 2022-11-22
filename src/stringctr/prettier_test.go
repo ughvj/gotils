7982cb433048fc164f612454871039c90e099342
ughvj <nessterday@gmail.com>
2022-11-23 00:39:20 +0900

    append test

--- src/stringctr/prettier_test.go
+++ src/stringctr/prettier_test.go
@@ -5,7 +5,32 @@ import (
 	"testing"
 )
 
-func Test_Prettier(t *testing.T) {
+func Test_Prettier_IgnoreEmptyIsTrue(t *testing.T) {
+	expected := `***   Hello, Golang worl   ***
+***   d and goodbye, Cla   ***
+***   ng world !!          ***
+`
+	prettier := NewPrettier()
+	actual, err := prettier.
+		Target("Hello, Golang world and goodbye, Clang world !!").
+		RowLimit(30).
+		TargetLimit(18).
+		Margin(3).
+		Padding("*").
+		IgnoreEmpty(true).
+		Prettify()
+
+	fmt.Printf("%s", actual)
+	if err != nil {
+		t.Error("something went wrong")
+	}
+	if actual != expected {
+		t.Error("actual is unexpected")
+	}
+}
+
+
+func Test_Prettier_IgnoreEmptyIsFalse(t *testing.T) {
 	expected := `***   Hello, Golang        ***
 ***   world and goodbye,   ***
 ***   Clang world !!       ***
